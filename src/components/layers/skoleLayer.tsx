f7fb3190672a422797356739519763cfc3cee459
robynemiliek <rokr012@student.kristiania.no>
2026-03-02 11:42:15 +0100

    style skoler based on their properties

--- src/components/layers/skoleLayer.tsx
+++ src/components/layers/skoleLayer.tsx
@@ -2,17 +2,34 @@ import VectorLayer from "ol/layer/Vector.js";
 import VectorSource from "ol/source/Vector.js";
 import { GeoJSON } from "ol/format.js";
 import { Circle, Fill, Stroke, Style } from "ol/style.js";
+import { Feature } from "ol";
+import type { FeatureLike } from "ol/Feature.js";
+
+interface SkoleProperties {
+  antall_ansatte: number;
+  antall_elever: number;
+  eierforhold: "Offentlig" | "Privat";
+  navn: string;
+}
+
+function skoleStyle(feature: FeatureLike) {
+  const props = feature.getProperties() as SkoleProperties;
+
+  return new Style({
+    image: new Circle({
+      radius: 4 + props.antall_elever / 100,
+      stroke: new Stroke({ width: 2, color: "white" }),
+      fill: new Fill({
+        color: props.eierforhold === "Offentlig" ? "blue" : "purple",
+      }),
+    }),
+  });
+}
 
 export const skoleLayer = new VectorLayer({
   source: new VectorSource({
     url: "/skoler.geojson",
     format: new GeoJSON(),
   }),
-  style: new Style({
-    image: new Circle({
-      radius: 4,
-      stroke: new Stroke({ width: 2, color: "white" }),
-      fill: new Fill({ color: "blue" }),
-    }),
-  }),
+  style: skoleStyle,
 });
