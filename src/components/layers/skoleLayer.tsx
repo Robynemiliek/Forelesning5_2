9972fd2fa874711e27e5782748c40df7e910ecf2
robynemiliek <rokr012@student.kristiania.no>
2026-03-02 11:33:00 +0100

    style skoler

--- src/components/layers/skoleLayer.tsx
+++ src/components/layers/skoleLayer.tsx
@@ -1,10 +1,18 @@
 import VectorLayer from "ol/layer/Vector.js";
 import VectorSource from "ol/source/Vector.js";
 import { GeoJSON } from "ol/format.js";
+import { Circle, Fill, Stroke, Style } from "ol/style.js";
 
 export const skoleLayer = new VectorLayer({
   source: new VectorSource({
     url: "/skoler.geojson",
     format: new GeoJSON(),
+  }),
+  style: new Style({
+    image: new Circle({
+      radius: 4,
+      stroke: new Stroke({ width: 2, color: "white" }),
+      fill: new Fill({ color: "blue" }),
+    }),
   }),
 });
